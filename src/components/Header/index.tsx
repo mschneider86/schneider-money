d5210a290f89ea57a2e8425144a0b710086f460c
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-20 19:21:05 -0300

    added new transaction modal initial structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Content } from './styles';
+
 import logoImg from '../../assets/logo.svg';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt='schneider money' />
-        <button type='button'>Nova transação</button>
+        <button type='button' onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
