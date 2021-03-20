6555882b0fdc67daee5c8ad1feff5fba396c2506
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-20 19:55:28 -0300

    updated modal styling

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import closeImg from '../../assets/close.svg';
 import Modal from 'react-modal';
 import { Container } from './styles';
 
@@ -12,7 +13,19 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={closeImg} alt='Fechar modal' />
+      </button>
       <Container>
         <h2>Cadastrar Transação</h2>
 
