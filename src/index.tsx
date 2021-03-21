b483fec86e22e12a32e83c68b5e181b543498515
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-21 13:18:08 -0300

    added fake transactions

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de App',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 8000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2021-02-02 11:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
